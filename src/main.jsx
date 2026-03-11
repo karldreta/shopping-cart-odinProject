e2e7c29e8f3727cdfb0a7d127da62ee20738d032
Karl James <karldreta@gmail.com>
2026-03-11 16:45:30 +0800

    Add Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/App.jsx'
+import NavBar from "./components/NavBar.jsx";
+import Product from './components/Shop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <NavBar/>
+    {/* <Product /> */}
   </StrictMode>,
 )
