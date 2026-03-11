af54f4dded590a739fe33e9c02b279b0fba93312
Karl James <karldreta@gmail.com>
2026-03-11 12:20:40 +0800

    Initial Commit, Edit Template, add useProductURL

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import useProductURL from '../utils/useProductURL'
+import './App.css'
+
+const Product = () => {
+  const { productName, productImage, productDescription, error, loading  } = useProductURL();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>A network error was encountered</p>;
+
+  return (
+    <>
+      <h1>{productName}</h1>
+      <img src={productImage} alt={"placeholder text"} />
+      <h2>{productDescription}</h2>
+    </>
+  );
+};
+
+export default Product
