e52ac63b2f09c6d08a3a382e772c67c4500e4ee4
Karl James <karldreta@gmail.com>
2026-03-11 18:21:37 +0800

    Add HomePage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,17 @@
+import '../styles/HomePage.css';
+
+const HomePage = () => {
+    return (
+        <main>
+            <div id="HeroSection">
+                <h1>You've reached a Checkpoint.</h1>
+                <p>
+                Take a moment to explore what we have in store. Browse our collection of essentials, unique finds, and everyday items chosen to make life a little easier. Whether you're looking for something practical or something new to try, there's always something worth discovering. Take your time, look around, and find what suits you best.
+                </p>
+                <h3><a href="/">Enter the Shop</a></h3>
+            </div> 
+        </main>
+    )
+}
+
+export default HomePage
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import NavBar from "./components/NavBar.jsx";
+import HomePage from './components/HomePage.jsx';
 import Product from './components/Shop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar/>
+    <HomePage/>
     {/* <Product /> */}
   </StrictMode>,
 )
