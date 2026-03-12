90c778dc34dc95924324f7b363db8711ed34747d
Karl James <karldreta@gmail.com>
2026-03-12 11:35:42 +0800

    Add catorgories section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,42 @@
 import '../styles/HomePage.css';
 
 const HomePage = () => {
+
+    
     return (
         <main>
-            <div id="HeroSection">
+            <section id="HeroSection">
                 <h1>You've reached a Checkpoint.</h1>
                 <p>
-                Take a moment to explore what we have in store. Browse our collection of essentials, unique finds, and everyday items chosen to make life a little easier. Whether you're looking for something practical or something new to try, there's always something worth discovering. Take your time, look around, and find what suits you best.
+                Take a moment to explore what we have in store. From everyday clothing to practical accessories and personal essentials, our collection is designed to fit your daily routine. Whether you're refreshing your wardrobe or picking up something simple for the day ahead, you'll find pieces that combine comfort, style, and usefulness. Take your time, browse around, and discover something that suits you.
                 </p>
-                <h3><a href="/">Enter the Shop</a></h3>
-            </div> 
+                <h3><a href="/">Check out Shop</a></h3>
+            </section> 
+            <section id="CategoriesSection">
+                <h3>Browse by Category</h3>
+                <ul>
+                    <li>
+                        <a href="#"></a>
+                        <div className='iconWrapper'>
+
+                        </div>
+                        <span>Men's</span>
+                    </li>
+                    <li> 
+                        <a href="#"></a>
+                        <div className='iconWrapper'>   
+                        </div>
+                        <span>Women's</span>
+                    </li>
+                    <li>
+                    <a href="#"></a>
+                        <div className='iconWrapper'>   
+                        </div>
+                        <span>Everything else</span>
+                    </li>
+                </ul>
+            </section>
+
         </main>
     )
 }
